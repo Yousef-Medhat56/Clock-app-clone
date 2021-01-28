3bcca5a36d54d65cad5dd0f88ad97b19df01551e
Yousef-Medhat56 <yousif.medhat562004@gmail.com>
2021-01-28 20:14:07 +0200

    Adding the event listeners

--- main/app.js
+++ main/app.js
@@ -1,75 +1,93 @@
-//selectors
+//SELECTORS
 
-const hundredthSecond = document.getElementById("hundredth-of-second")
-const startBtn = document.getElementById("start-btn")
-const second = document.getElementById("second")
-const seconDot = document.getElementById("second-dot")
-const secondZero = document.getElementById("second-zero")
-const afterStart = document.getElementById("btn-div-1")
-const minute = document.getElementById("minute")
-const minuteZero = document.getElementById("minute-zero")
-
-const hour = document.getElementById("hour")
-const hourZero = document.getElementById("hour-zero")
+//SELECTORS - Buttons
+const startStopBtn = document.getElementById("start-btn") //Start and Stop buttons
+const afterStartBtn = document.getElementById("after-start-btn") //<DIV> That contains resume and rest buttons 
+const resumeBtn = document.getElementById("resume-btn") //Resume button
+const resetBtn = document.getElementById("reset-btn") //Reset button
 
 
-const stopResumeBtn = document.getElementById("pause-btn")
-const lapBtn = document.getElementById("lap-btn")
+//SELECTORS - Time elements
+const hundredthSecond = document.getElementById("hundredth-of-second") //hundredth-of-seconds
+const second = document.getElementById("second") //Seconds
+const secondZero = document.getElementById("second-zero") //the zero before seconds
+const minute = document.getElementById("minute") //Minutes
+const minuteZero = document.getElementById("minute-zero") //the zero before minutes
+const hour = document.getElementById("hour") //Hours
+const hourZero = document.getElementById("hour-zero") //the zero before hours
+
+
+
 //Variables
-var hundredSec = 0
+let hundredSec = 0 //hundredth-of-seconds
+let sec = 0 //seconds
+let min = 0 //minutes
+let hr = 0 //hours
 
-var sec = 0
-var hr = 0
-var min = 0
 //functions names
-var stopwatch;
+let stopwatch; //The setInterval function()
 
 
-//eventlisteners
+//Event listeners
 
-startBtn.addEventListener("click" ,function(){
-    clickStart()
-    afterStart.style.display = "flex"
-    startBtn.style.display = "none"
+//Clicking Start or Stop buttons
+startStopBtn.addEventListener("click" ,function(){
+    
+    //Clicking Start button
+    if (startStopBtn.textContent == "Start"){
+        clickStart()
+        startStopBtn.textContent = "Stop"
+        startStopBtn.style.color ="red"
+        startStopBtn.style.outlineColor ="red"
+    }
+
+    //Clicking Stop buttons
+    else if(startStopBtn.textContent == "Stop"){
+        clickStop()
+        afterStartBtn.style.display = "flex"
+        startStopBtn.style.display = "none"
+        startStopBtn.style.outlineColor ="rgb(48, 190, 143)"
+    }
+    
 })
 
-
-stopResumeBtn.addEventListener("click",function(){
-    if (stopResumeBtn.textContent == "Stop"){
-        clickStop()
-    }
-    else if (stopResumeBtn.textContent == "Resume"){
-        clickResume()
-    }
+//Clicking resume button
+resumeBtn.addEventListener("click",function(){
+    clickResume()
 }) 
-//functions
+//Clicking Reset button
+resetBtn.addEventListener("click" , clickRestart)
+
+//Functions
+
 
 function clickStart(){
-    stopwatch = setInterval(stopwatCounter,10)
+    stopwatch = setInterval(stopwatCounter,10) //Counting the hundredth of seconds
 }
 
-
+//making the time elements change
 function stopwatCounter(){
     //HUNDREDTH SECONDS
     hundredSec++ //Increasing the hundredth seconds 
-    hundredthSecond.textContent = hundredSec 
+    hundredthSecond.textContent = hundredSec //showing the hundredth seconds 
     hundredthSecond.style.opacity=1 //Making them obvious
     
     //Seconds
-    if(hundredSec >99){
+    if(hundredSec >98){
         hundredSec= 0 
+        
         sec++ //Increasing the  seconds 
         second.textContent = sec + "." //showing the seconds 
         second.style.opacity = 1 //Making them obvious
         
 
         //removing zero before second sections if seconds >9
-        if(sec>9 && sec<59){
+        if(sec>9 && sec<60){
             secondZero.style.display = "none"
             
         }
         //Minutes
-        else if(sec==59){
+        else if(sec==60){
             sec = 0
             second.textContent = sec + "."
             //returning the zero before the second value if seconds < 9
@@ -121,16 +139,46 @@ function stopwatCounter(){
 
 function clickStop(){
     clearInterval(stopwatch)
-    stopResumeBtn.textContent = "Resume"
-    stopResumeBtn.style.color = "rgb(48, 190, 143)"
-    lapBtn.textContent = "Reset"
+    clearInterval(lapStopwatch)
+    resumeBtn.textContent = "Resume"
+    resumeBtn.style.color = "green"
+    resetBtn.textContent = "Reset"
 
 }
 
 function clickResume(){
     stopwatch =setInterval(stopwatCounter,10)
-    stopResumeBtn.textContent = "Stop"
-    stopResumeBtn.style.color = "red"
-    lapBtn.textContent = "Lap"
+    
+    startStopBtn.style.outlineColor ="red"
+    afterStartBtn.style.display = "none"
+    startStopBtn.style.display = "block"
 }
 
+
+
+function clickRestart(){
+    startStopBtn.style.outlineColor ="rgb(48, 190, 143)"
+    hundredSec =0
+    sec = 0
+    min = 0
+    hr = 0
+    hundredthSecond.textContent = "00"
+    second.textContent = "0."
+    minute.textContent = "0:"
+    
+    
+    clearInterval(stopwatch)
+    afterStartBtn.style.display = "none"
+    startStopBtn.style.display = "block"
+    startStopBtn.textContent = "Start"
+    startStopBtn.style.color = "rgb(48, 190, 143)"
+    
+    hundredthSecond.style.opacity = 0.5
+    second.style.opacity = 0.5
+    secondZero.style.opacity = 0.5
+    minute.style.opacity = 0.5
+    minuteZero.style.opacity = 0.5
+    hour.style.display = "none"
+    hourZero.style.display = "none"
+    
+}
